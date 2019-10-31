cb108ab9331dd287542165e4f5f95704c14ea6a3
acerjt <acersjt@gmail.com>
2019-10-31 13:26:31 +0700

    login_admin

---
+++ config/dbconfig.php
@@ -0,0 +1,10 @@
+﻿<?php
+	$tenmaychu="localhost";
+	$tentaikhoan="root";
+	$pass="DungNgoc@261297";
+	$csdl="firstdb";
+	$site_url = 'http://localhost:80/webproject/';
+	$site_admin = 'http://localhost:80/webproject/admin/';
+	$conn=mysqli_connect($tenmaychu, $tentaikhoan, $pass, $csdl) or die("Không kết nối được");
+	mysqli_set_charset($conn,"utf8");
+?>
