581ffd3c89d8405180d3a0002e07bd6b4406c7a2
DungNgoc <you@example.com>
2019-11-03 21:40:17 +0700

    header done.

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -1,10 +1,10 @@
 ﻿<?php
 	$tenmaychu="localhost";
 	$tentaikhoan="root";
-	$pass="DungNgoc@261297";
-	$csdl="firstdb";
-	$site_url = 'http://localhost:80/webproject/';
-	$site_admin = 'http://localhost:80/webproject/admin/';
+	$pass="";
+	$csdl="mydatabase";
+	$site_url = 'http://localhost:80/SE347.J11.PMCL/';
+	$site_admin = 'http://localhost:80/SE347.J11.PMCL/admin/';
 	$conn=mysqli_connect($tenmaychu, $tentaikhoan, $pass, $csdl) or die("Không kết nối được");
 	mysqli_set_charset($conn,"utf8");
 ?>
